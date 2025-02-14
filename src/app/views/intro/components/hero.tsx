abdd2e4af2d046dae706d4c7ce3d7292db6d7c0c
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-02-14 22:54:51 +0100

    hotfix apostroph

--- src/app/views/intro/components/hero.tsx
+++ src/app/views/intro/components/hero.tsx
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 
 import Link from "next/link";
 
@@ -7,7 +7,7 @@ export default async function Hero() {
 
     return (
         <div className="flex flex-col gap-2">
-            <h1 className="text-[48px] font-bold"> Hi, I'm Noah </h1>
+            <h1 className="text-[48px] font-bold"> Hi, I&apos;m Noah </h1>
             <p className="text-accent font-bold text-xl"> Full Stack Web Developer </p>
             <Link
                 className="p-4 bg-accent rounded-full text-center text-2xl font-bold text-black"
